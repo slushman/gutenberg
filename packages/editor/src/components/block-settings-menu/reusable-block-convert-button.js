a7689cbf6ec98b72822c752a4ae93464c565ed08
Robert Anderson <robert@noisysocks.com>
2018-10-11 12:42:36 +1100

    Hide 'Add to Reusable Blocks' button when 'core/block' is disabled
    Hides the 'Add to Reusable Block' buton when 'core/block' is not in the
    array returned by the `allowed_block_types` filter.

--- packages/editor/src/components/block-settings-menu/reusable-block-convert-button.js
+++ packages/editor/src/components/block-settings-menu/reusable-block-convert-button.js
@@ -1,7 +1,7 @@
 /**
  * External dependencies
  */
-import { noop, every, map } from 'lodash';
+import { noop, some, map } from 'lodash';
 
 /**
  * WordPress dependencies
@@ -49,25 +49,33 @@ export function ReusableBlockConvertButton( {
 
 export default compose( [
 	withSelect( ( select, { clientIds } ) => {
-		const { getBlock, getReusableBlock } = select( 'core/editor' );
+		const { getBlock, canInsertBlockType, getReusableBlock } = select( 'core/editor' );
 		const { getFallbackBlockName } = select( 'core/blocks' );
 
 		const blocks = map( clientIds, ( clientId ) => getBlock( clientId ) );
+		if ( some( blocks, ( block ) => ! block ) ) {
+			return { isVisible: false };
+		}
 
 		// Hide 'Add to Reusable Blocks' on Classic blocks. Showing it causes a
 		// confusing UX, because of its similarity to the 'Convert to Blocks' button.
-		const isVisible = (
-			every( blocks, ( block ) => !! block ) &&
-			( blocks.length !== 1 || blocks[ 0 ].name !== getFallbackBlockName() )
-		);
+		if ( blocks.length === 1 && blocks[ 0 ].name === getFallbackBlockName() ) {
+			return { isVisible: false };
+		}
+
+		// Hide 'Add to Reusable Blocks' when Reusable Blocks are disabled, i.e. when
+		// core/block is not in the allowed_block_types filter.
+		if ( ! canInsertBlockType( 'core/block' ) ) {
+			return { isVisible: false };
+		}
 
 		return {
-			isStaticBlock: isVisible && (
+			isVisible: true,
+			isStaticBlock: (
 				blocks.length !== 1 ||
 				! isReusableBlock( blocks[ 0 ] ) ||
 				! getReusableBlock( blocks[ 0 ].attributes.ref )
 			),
-			isVisible,
 		};
 	} ),
 	withDispatch( ( dispatch, { clientIds, onToggle = noop } ) => {
