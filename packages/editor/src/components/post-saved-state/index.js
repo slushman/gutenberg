e690f5392887645b41a50ad89bffa54a4363a422
jasmussen <joen@automattic.com>
2018-10-12 13:37:59 +0200

    Polish Save Draft/Switch to Draft area
    - Adds a new TextButton component, for tertiary actions.
    - Makes saved state look disabled, because it's inactionable. Fixes #10415, maybe fixes #9452
    - Hide cloud icon on desktop breakpoints. Fixes #9455
    - Makes "Switch to Draft" button into the new tertiary style of buttons.

--- packages/editor/src/components/post-saved-state/index.js
+++ packages/editor/src/components/post-saved-state/index.js
@@ -7,7 +7,7 @@ import classnames from 'classnames';
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { Dashicon, IconButton } from '@wordpress/components';
+import { Dashicon, TextButton } from '@wordpress/components';
 import { Component } from '@wordpress/element';
 import { withSelect, withDispatch } from '@wordpress/data';
 import { displayShortcut } from '@wordpress/keycodes';
@@ -77,14 +77,14 @@ export class PostSavedState extends Component {
 		}
 
 		return (
-			<IconButton
+			<TextButton
 				className="editor-post-save-draft"
 				onClick={ onSave }
 				icon="cloud-upload"
 				shortcut={ displayShortcut.primary( 's' ) }
 			>
 				{ __( 'Save Draft' ) }
-			</IconButton>
+			</TextButton>
 		);
 	}
 }
